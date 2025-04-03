cd4ecfd4f184ba6fe98c3a05afa3f5b186329652
Richard Esmond <richard.esmond@gmail.com>
2025-04-03 05:37:25 -0500

    refactored key analysis into KeyGraph

--- src/gengraphlib/GrabBag/JsonReader.py
+++ src/gengraphlib/GrabBag/JsonReader.py
@@ -1,11 +1,11 @@
 import os
 import json
-import JsonAnalysis as ja
+from src.gengraphlib.logs import KeyGraph as ja
 from progress.bar import Bar
 
 def read_json(filepath: str):
 
-    try:
+ #   try:
         all_keys: dict[str, int] = {}
 
         bar = Bar("Processing", max=125145)
@@ -13,15 +13,16 @@ def read_json(filepath: str):
 
             for line in file:
                 obj = json.loads(line)
-                for key, value in obj.items():
-                    if key not in all_keys:
-                        all_keys[key] = 1
+                for key, values_set in obj.items():
+                    if key not in ja.log_to_json:
+                        if key not in all_keys:
+                            all_keys[key] = 1
+                        else:
+                            all_keys[key] += 1
                     else:
-                        all_keys[key] += 1
-
-                    json_name = ja.log_to_json[key]
-                    if json_name and ja.capture_flags[key]:
-                        ja.print_dict[json_name].append(value)
+                        json_name = ja.log_to_json[key]
+                        if json_name and ja.capture_flags[key]:
+                            ja.values_bykeys_set[json_name ].append( values_set )
 
                 bar.next()
 
@@ -29,23 +30,28 @@ def read_json(filepath: str):
 
         flipped_keys: dict[int, str] = {}
 
-        for key, value in all_keys.items():
-            flipped_keys[value] = key
+        for key, values_set in all_keys.items():
+            flipped_keys[values_set] = key
 
         sorted_keys: dict[str,int] = {}
-        for key, value in sorted(flipped_keys.items()):
-            sorted_keys[value] = key
+        for key, values_set in sorted(flipped_keys.items()):
+            sorted_keys[values_set] = key
 
         data_str  = json.dumps(sorted_keys, indent=4)
         print(data_str)
-        open( "/home/richard/data/jctl-logs/keys/keys.json", "w+" ).write( data_str )
+        open( "/home/richard/data/jctl-logs/keys/missedkeys.json", "w" ).write( data_str )
 
-        for key, value in ja.print_dict.items():
-            key_str = json.dumps(value, indent=4)
+        for key, values_set in ja.values_bykeys_set.items():
+            if not os.path.exists(f"/home/richard/data/jctl-logs/keys/{key}"):
+                os.mkdir(f"/home/richard/data/jctl-logs/keys/{key}")
 
-            with open(f'/home/richard/data/jctl-logs/keys/{key}.json', key_str) as keyfile:
-                keyfile.write(key_str)
+            with open(f'/home/richard/data/jctl-logs/keys/{key}/{key}.json', "w") as keyfile:
+                for value_line in values_set:
+                    value_line_json = json.dumps(value_line, indent=4)
+                    keyfile.write(value_line_json)
+                    keyfile.write("\n")
 
+"""
         for key, value in ja.capture_flags:
             if value:
                 os.mkdir(f'/home/richard/data/jctl-logs/keys/{key}')
@@ -53,12 +59,12 @@ def read_json(filepath: str):
                     keyline = ja.log_to_json[key]
                     key_str = json.dumps(keyline, indent=4)
                     keyfile.write(key_str)
+"""
 
 
 
-
-    except Exception as e:
-        print(f"Exception: {e}")
+#    except Exception as e:
+#        print(f"Exception: {e}")
 
 if __name__ == "__main__":
     os.curdir = "/home/richard/data/jctl-logs/"
